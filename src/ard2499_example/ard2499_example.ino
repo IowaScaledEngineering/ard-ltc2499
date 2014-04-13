0db87b35541575e3b3e2dca891c894dc9960f481
Nathan D. Holmes <maverick@drgw.net>
2014-04-13 22:40:40 +0000

    Update LTC2499 example code

--- src/ard2499_example/ard2499_example.ino
+++ src/ard2499_example/ard2499_example.ino
@@ -1,24 +1,3 @@
-/*************************************************************************
-Title:    ARD-LTC2499 Library Example Arduino Sketch
-Authors:  Nathan D. Holmes <maverick@drgw.net>
-File:     $Id: $
-License:  GNU General Public License v3
-
-LICENSE:
-    Copyright (C) 2013 Nathan D. Holmes & Michael D. Petersen
-
-    This program is free software; you can redistribute it and/or modify
-    it under the terms of the GNU General Public License as published by
-    the Free Software Foundation; either version 2 of the License, or
-    any later version.
-
-    This program is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU General Public License for more details.
-
-*************************************************************************/
-
 #include <Wire.h>
 #include <Ard2499.h>
 
@@ -32,9 +11,9 @@ void setup() {
   Wire.begin();
   ard2499board1.begin(ARD2499_ADC_ADDR_ZZZ, ARD2499_EEP_ADDR_ZZ);
   ard2499board1.ltc2499ChangeConfiguration(LTC2499_CONFIG2_60_50HZ_REJ);
-  confChan = ard2499board1.ltc2499ChangeChannel(LTC2499_CHAN_TEMPERATURE);
-}
+  ard2499board1.ltc2499ChangeChannel(LTC2499_CHAN_SINGLE_0P);
 
+}
 byte i=0;
 
 void loop() {
@@ -58,16 +37,102 @@ void loop() {
   Serial.print(ard2499board1.eepromRead(0, true));
   Serial.print("\n");
 
-  ard2499board1.ltc2499ChangeChannel(LTC2499_CHAN_SINGLE_0P);
-  Serial.print("Channel 0 SE");
-  Serial.print(" = [");
-  Serial.print(ard2499board1.ltc2499ReadAndChangeChannel(LTC2499_CHAN_TEMPERATURE));
-  Serial.print("]\n");
 
-  Serial.print("Temp = [");
-  Serial.print(ard2499board1.ltc2499ReadTemperature(ARD2499_TEMP_F));
-  Serial.print(" F]\n");
-  
+  for(i=0; i<17; i++)
+  {
+    byte newChannel;
+    
+    switch(i)
+    {
+       case 0:
+          newChannel = LTC2499_CHAN_SINGLE_1P;
+          break;
+          
+       case 1:
+          newChannel = LTC2499_CHAN_SINGLE_2P;
+          break;
+
+       case 2:
+          newChannel = LTC2499_CHAN_SINGLE_3P;
+          break;
+
+       case 3:
+          newChannel = LTC2499_CHAN_SINGLE_4P;
+          break;
+
+       case 4:
+          newChannel = LTC2499_CHAN_SINGLE_5P;
+          break;
+          
+       case 5:
+          newChannel = LTC2499_CHAN_SINGLE_6P;
+          break;
+
+       case 6:
+          newChannel = LTC2499_CHAN_SINGLE_7P;
+          break;
+
+       case 7:
+          newChannel = LTC2499_CHAN_SINGLE_8P;
+          break;
+
+       case 8:
+          newChannel = LTC2499_CHAN_SINGLE_9P;
+          break;
+          
+       case 9:
+          newChannel = LTC2499_CHAN_SINGLE_10P;
+          break;
+
+       case 10:
+          newChannel = LTC2499_CHAN_SINGLE_11P;
+          break;
+
+       case 11:
+          newChannel = LTC2499_CHAN_SINGLE_12P;
+          break;
+
+       case 12:
+          newChannel = LTC2499_CHAN_SINGLE_13P;
+          break;
+
+       case 13:
+          newChannel = LTC2499_CHAN_SINGLE_14P;
+          break;
+
+       case 14:
+          newChannel = LTC2499_CHAN_SINGLE_15P;
+          break;
+
+       case 15:
+          newChannel = LTC2499_CHAN_TEMPERATURE;
+          break;
+
+       case 16:
+       default:
+          newChannel = LTC2499_CHAN_SINGLE_0P;
+          break;
+
+    }
+
+    if (i == 16)
+    {
+      Serial.print("LTC2499 Temperature");
+      Serial.print(" = [");
+      Serial.print(ard2499board1.ltc2499ReadTemperature(ARD2499_TEMP_F));
+      ard2499board1.ltc2499ChangeChannel(newChannel);
+      Serial.print(" F]\n");
+    }
+    else
+    {
+      Serial.print("Channel ");
+      Serial.print(i);
+      Serial.print(" SE");
+      Serial.print(" = [");
+      Serial.print(ard2499board1.ltc2499ReadAndChangeChannel(newChannel));
+      Serial.print("]\n");
+    }
+  }
   
   delay(1000);
 }
